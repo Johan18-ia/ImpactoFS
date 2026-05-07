64351d31a1e6a1f96ee0fa96adb146e913c7b4d1
lolmander3 <jolmanjhon@gmail.com>
2026-05-07 15:40:48 -0500

    re-ajuste de la ip en swagger

--- Impacto_FS/frontend_web/backend/config/swagger.js
+++ Impacto_FS/frontend_web/backend/config/swagger.js
@@ -13,7 +13,7 @@ const options = {
     },
     servers: [
       {
-        url: 'http://192.168.1.77:3000',
+        url: 'http://192.168.56.1:3000',
         description: 'Servidor local'
       }
     ],
