cacc897e6bb75c256f2d489dd24f9c269476076d
lolmander3 <jolmanjhon@gmail.com>
2026-05-08 15:39:20 -0500

    comentareado para el db_node y el agergado para crear una tabla de productos

--- Impacto_FS/frontend_web/database/db_node.sql
+++ Impacto_FS/frontend_web/database/db_node.sql
@@ -1,9 +1,15 @@
+/* Crea la base de datos en SQL */
+--(en caso de que exista una base de datos con el mismo nombre la elimina).
 DROP DATABASE IF EXISTS db_node;
-
 CREATE SCHEMA db_node DEFAULT CHARACTER SET utf8 ;
-USE db_node;
-CREATE TABLE users (
-id INT AUTO_INCREMENT PRIMARY KEY,
+
+
+/* Crea la tabla para los Usuarios*/
+
+USE db_node; --usar la base de datos
+
+CREATE TABLE users ( 
+id INT AUTO_INCREMENT PRIMARY KEY, --es int de momento pero deberia ser BIGINT 
 name VARCHAR(100) NOT NULL,
 lastname VARCHAR(100) NOT NULL,
 email VARCHAR(150) NOT NULL UNIQUE,
@@ -14,6 +20,8 @@ role VARCHAR(20),
 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB;
+
+-- Insertar un usuario por SQL
 INSERT INTO users VALUES (
 null,
 "Albeiro",
@@ -25,4 +33,21 @@ null,
 "admin",
 null,
 null
+);
+
+/* Crea la tabla para los productos*/
+
+USE db_node;
+
+CREATE TABLE productos(
+    id BIGINT PRIMARY KEY AUTO_INCREMENT, -- BIGINT porque pueden existir muchos productos
+    nombre VARCHAR(255) NOT NULL,
+    descripcion TEXT, -- La descripción no es obligatoria
+    precio DOUBLE NOT NULL,
+    stock INT NOT NULL, -- Cantidad disponible de productos
+    imagen VARCHAR(255), -- URL o nombre de la imagen del producto
+    categoria VARCHAR(100),
+    fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    fecha_actualizacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        ON UPDATE CURRENT_TIMESTAMP
 );
