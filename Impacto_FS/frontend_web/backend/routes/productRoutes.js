6e4cec4adb58e441bdf80a444146d45a15e916ad
lolmander3 <jolmanjhon@gmail.com>
2026-05-12 17:15:57 -0500

    swagger productos

--- Impacto_FS/frontend_web/backend/routes/productRoutes.js
+++ Impacto_FS/frontend_web/backend/routes/productRoutes.js
@@ -1,58 +1,45 @@
 // Importar express
 const express = require('express');
+
 // Crear router
 const router = express.Router();
+
 // Importar controlador
 const productController = require('../controllers/productController');
+
 // Importar middlewares
 const {
     verifyToken,
     authorizeRoles
 } = require('../middlewares/authMiddleware');
+
+
 // ============================================
-// LISTAR PRODUCTOS
+// SWAGGER + RUTAS
 // ============================================
+
+/**
+ * @swagger
+ * tags:
+ *   name: Products
+ *   description: Gestión de productos
+ */
+
+
+/**
+ * @swagger
+ * /api/products:
+ *   get:
+ *     tags: [Products]
+ *     summary: Obtener todos los productos
+ */
 router.get(
     '/',
     verifyToken,
     authorizeRoles(['admin', 'seller']),
     productController.getAllProducts
 );
-// ============================================
-// OBTENER PRODUCTO POR ID
-// ============================================
-router.get(
-    '/:id',
-    verifyToken,
-    authorizeRoles(['admin', 'seller']),
-    productController.getProductById
-);
-// ============================================
-// CREAR PRODUCTO
-// ============================================
-router.post(
-    '/create',
-    verifyToken,
-    authorizeRoles(['admin', 'seller']),
-    productController.create
-);
-// ============================================
-// ACTUALIZAR PRODUCTO
-// ============================================
-router.put(
-    '/',
-    verifyToken,
-    authorizeRoles(['admin', 'seller']),
-    productController.update
-);
-// ============================================
-// ELIMINAR PRODUCTO
-// ============================================
-router.delete(
-    '/delete/:id',
-    verifyToken,
-    authorizeRoles(['admin']),
-    productController.delete
-);
-// Exportar rutas
-module.exports = router;
+
+
+
+// MÁS ENDPOINTS...
